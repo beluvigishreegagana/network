32b7bfb08946add76787ca9b065b80d964e6c6a0
beluvigishreegagana <beluvigishreegagana@gmail.com>
2024-06-06 12:11:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "subnet" {
+  count               = length(var.subnet_address_prefix)
+  name                = "subnet-${count.index + 1}"
+  resource_group_name = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes    = [element(var.subnet_address_prefix, count.index)]
+}
